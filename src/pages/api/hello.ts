d27e1f01ecb2925428fd5b5aa57cfac099a34724
hadiid-studentcode <hadiidandri2000@gmail.com>
2024-01-03 17:42:05 +0700

    new file:   src/lib/firebase/init.ts 	new file:   src/lib/firebase/service.ts 	modified:   src/pages/api/hello.ts 	modified:   src/pages/api/product.ts

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,14 +1,14 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
 
 type Data = {
   name: string;
   age: number;
-}
+};
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'Hadiid Andri Yulison',age:23 })
+  res.status(200).json({ name: "Hadiid Andri Yulison", age: 23 });
 }
--- src/pages/api/product.ts
+++ src/pages/api/product.ts
@@ -1,35 +1,18 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { retrieveData } from "@/lib/firebase/service";
 import type { NextApiRequest, NextApiResponse } from "next";
 
 type Data = {
   status: boolean;
   statusCode: number;
-  data: {
-    id: number;
-    name: string;
-    price: number;
-    size: string;
-  }[];
+  data: any;
 };
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  const data = [
-    {
-      id: 1,
-      name: "Baju Kemeja",
-      price: 500000,
-      size: "xl",
-    },
-    {
-      id: 2,
-      name: "Baju Koko",
-      price: 100000,
-      size: "l",
-    },
-  ];
+  const data = await retrieveData("products");
 
   res.status(200).json({ status: true, statusCode: 200, data });
 }
